b97275c82cb9f45c6a2692ec253ca37f97b85e44
khayes133 <kjhayes9@gmail.com>
2024-02-22 00:50:38 -0700

    moving things around

--- index.js
+++ index.js
@@ -1,46 +1,94 @@
 const express = require('express');
-const app = express();
-
-// Import the axios library, to make HTTP requests
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const GithubStrategy = require('passport-github2').Strategy;
 const axios = require('axios');
 
-// This is the client ID and client secret that you obtained
-// while registering the application
-const clientID = process.env.GITHUB_CLIENT_ID;
-const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+const api = express();
+const mongodb = require('./data/database');
 
-// Declare the redirect route
-app.get('/home', (req, res) => {
+// API 'use' Setup
+api.use(bodyParser.json());
+api.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+api.use(passport.initialize());
+api.use(passport.session());
+api.use(
+    cors({
+        methods: ['POST', 'PUT', 'GET', 'DELETE'],
+        origin: '*',
+        allowedHeaders: [
+            'Origin',
+            'X-Requested-With',
+            'Content-Type',
+            'Accept',
+            'Z-Key',
+            'Authorization',
+        ],
+    })
+);
+api.use(express.json());
+api.use(express.urlencoded({ extended: true }));
 
-  // The req.query object has the query params that were sent to this route.
-  const requestToken = req.query.code;
-  
-  axios({
-    method: 'post',
-    url: `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSON
-    headers: {
-         accept: 'application/json'
-    }
-    
-  }).then((response) => {
-    
-    const accessToken = response.data.access_token;
-    console.log(response.data);
-    
-    // Redirect the user to the home page, along with the access token
-    res.redirect(`/home.html?access_token=${accessToken}`);
-  }).catch((error) => {
-    // Handle errors
-    console.error('Error occurred while fetching access token:', error.response.data);
-    res.status(error.response.status || 500).send('Error occurred while fetching access token');
-  });
+// My Routes
+api.use('/', require('./routes'));
+
+// Passport Setup
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    )
+);
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
-// Serve static files from the 'public' directory
-app.use(express.static(__dirname + '/public'));
+// GitHub OAuth Redirect Route
+api.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
-// Start the server
-app.listen(4000, () => {
-    console.log("Server listening on port: 4000");
+// GitHub OAuth Callback Route
+api.get(
+    '/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    }
+);
+
+// Exception Catch
+process.on('uncaughtException', (err, origin) => {
+    console.log(
+        process.stderr.fd,
+        `Caught exception: ${err}\nException origin: ${origin}`
+    );
+});
+
+port = process.env.PORT || 8080;
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        api.listen(port, () => {
+            console.log('database listening on ' + port);
+        });
+    }
 });
