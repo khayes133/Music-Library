5c67d3d68bb86190ed229bc42dba5046e8201154
khayes133 <kjhayes9@gmail.com>
2024-02-21 21:26:18 -0700

    Moved things around for oauth to work.

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-const cors = require('cors');
 const express = require('express');
-const session = require('express-session');
-const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
-
+const mongodb = require('./data/database');
 const app = express();
-port = process.env.port || 8080;
 
-app.use(cors());
-app.use(express.json());
-app.use(session({ 
-    secret: process.env.SESSION_SECRET, 
-    resave: false, 
-    saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/', require('./routes'));
+const port = process.env.PORT || 3030;
+
+app.use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  next();
+})
+.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+.use(cors({ origin: '*' }))
+.use('/', require('./routes/index.js'));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+
+function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get('/github/callback', passport.authenticate('github', 
+    { failureRedirect: '/api-docs', session: false }), 
+    (req, res) => { 
+      req.session.user = req.user; 
+      res.redirect('/'); 
+    });
+
 
 mongodb.initDb((err) => {
     if(err) {
