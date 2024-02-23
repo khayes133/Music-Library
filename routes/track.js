8bcf15d8e2c3ae75dfbc0e6af9c7b3efc373642d
khayes133 <kjhayes9@gmail.com>
2024-02-22 22:52:30 -0700

    _

--- routes/track.js
+++ routes/track.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const trackController = require('../controllers/track');
-const { isAuthenticated } = require('../middleware/authenticate');
-const validation = require('../middleware/validate');
+//const { isAuthenticated } = require('../middleware/authenticate');
+//const validation = require('../middleware/validate');
 
 router.get('/', trackController.getAllTracks);
 
 router.get('/:id', trackController.getSingleTrack);
 
-router.post('/', validation.validateTrack, trackController.createTrack);
+router.post('/', trackController.createTrack);
 
-router.put('/:id', validation.validateTrack, trackController.updateTrack);
+router.put('/:id', trackController.updateTrack);
 
 router.delete('/:id', trackController.deleteTrack);
 
--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user');
-const { isAuthenticated } = require('../middleware/authenticate');
-const validation = require('../middleware/validate');
+//const { isAuthenticated } = require('../middleware/authenticate');
+//const validation = require('../middleware/validate');
 
 router.get('/', userController.getAllUsers);
 
 router.get('/:id', userController.getSingleUser);
 
-router.post('/', validation.validateUser, userController.createUser);
+router.post('/', userController.createUser);
 
-router.put('/:id', validation.validateUser, userController.updateUser);
+router.put('/:id',  userController.updateUser);
 
 router.delete('/:id', userController.deleteUser);
 
